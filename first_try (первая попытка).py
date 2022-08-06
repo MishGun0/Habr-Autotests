2c9c6d0d8b555c862021d85a4213182c53480d20
Titov Mikhail <mishanyatitov@gmail.com>
2022-08-06 15:12:11 +0400

    запуск поиска

--- first_try (первая попытка).py
+++ first_try (первая попытка).py
@@ -1,9 +1,17 @@
-from selenium.webdriver.chrome import webdriver
 import time
+
+from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.WebDriver("chromedriver")
 
-driver.get ("https://habr.com/ru/all/")
+driver.get("https://habr.com/ru/all/")
+
+time.sleep(5)
+
+locator = '[class="tm-svg-img tm-header-user-menu__icon tm-header-user-menu__icon_search"]'
+element = driver.find_element(By.CSS_SELECTOR, locator)
+element.click()
 
 time.sleep(5)
 
