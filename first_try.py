6cb57641722e6605fa51fef585b3a4403ca68c0f
Titov Mikhail <mishanyatitov@gmail.com>
2022-08-10 13:32:43 +0400

    Reformat code

--- first_try.py
+++ first_try.py
@@ -6,41 +6,33 @@ from selenium.webdriver.common.by import By
 driver = webdriver.WebDriver('chromedriver')
 
 driver.get('https://habr.com/ru/all/')
-
 time.sleep(3)
 
 # Открыть поиск
-
 search_button_locator = By.CSS_SELECTOR, 'a.tm-header-user-menu__item'
 search_button = driver.find_element(*search_button_locator)
 search_button.click()
-
 time.sleep(3)
 
 # вбить текст
-
 search_input_locator = By.CLASS_NAME, 'tm-input-text-decorated__input'
 search_input = driver.find_element(*search_input_locator)
 text_to_search = 'Selenium'
 search_input.send_keys(text_to_search)
 
 # нажать на иконку поиска
-
 search_icon_locator = By.CSS_SELECTOR, 'span.tm-search__icon'
 search_icon = driver.find_element(*search_icon_locator)
 search_icon.click()
-
 time.sleep(3)
-# посчитать количество записей (20)
 
+# посчитать количество записей (20)
 article_locator = By.TAG_NAME, 'article'
 articles = driver.find_elements(*article_locator)
 print(f'Number of articles is{len(articles)})')
-
-time.sleep(3)
+time.sleep(1)
 
 # посчитать количество страниц (50)
-
 last_page_locator = By.XPATH, '(//*[@class="tm-pagination__page"])[last()]'
 last_page_number = driver.find_element(*last_page_locator)
 element_text = last_page_number.text
