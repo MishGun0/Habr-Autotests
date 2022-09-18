e5cf1ae7f3541bed933202fdeb315e6c6595eec8
Titov Mikhail <mishanyatitov@gmail.com>
2022-09-18 15:24:05 +0400

    group locators

--- locators.py
+++ locators.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.common.by import By
 
+# Common locators
 last_page_locator = By.XPATH, '(//*[@class="tm-pagination__page"])[last()]'
 article_locator = By.TAG_NAME, 'article'
+search_button_locator = By.CSS_SELECTOR, 'a.tm-header-user-menu__item'
+
+# Search Page
 search_icon_locator = By.CSS_SELECTOR, 'span.tm-search__icon'
 search_input_locator = By.CLASS_NAME, 'tm-input-text-decorated__input'
-search_button_locator = By.CSS_SELECTOR, 'a.tm-header-user-menu__item'
 empty_res_locator = By.CLASS_NAME, 'tm-empty-placeholder__text'
